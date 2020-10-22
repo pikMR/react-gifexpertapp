442efeb321284bcde7f38f9ce39641cbd3eaded9
Benjamín García <benjahergar@gmail.com>
2020-10-22 23:18:51 +0200

    corregir rutas absolutas

---
+++ docs/precache-manifest.0c39b3b4541de1b331a6220e13f624d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d318ce2a9c352c7250d84004d70d268",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f0c9de5640800487eda",
+    "url": "/static/css/main.c10ebe8e.chunk.css"
+  },
+  {
+    "revision": "9d0467bdd98391920114",
+    "url": "/static/js/2.b04e268c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b04e268c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f0c9de5640800487eda",
+    "url": "/static/js/main.b4c7f0fe.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
